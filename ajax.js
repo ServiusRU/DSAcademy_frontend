362897b7e51b5b1111fa306d16b651a3b2f0b4fb
ServiusRU <102586762+ServiusRU@users.noreply.github.com>
2022-04-04 09:09:21 +0500

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,26 @@
+function call() {
+	var msg   = $('#formForSend').serialize();
+	$.ajax({
+		type: 'POST',
+		//url: 'https://60376bfd5435040017722533.mockapi.io/formRej',
+		url: 'https://60376bfd5435040017722533.mockapi.io/form',
+		data: msg,
+		success: function(data) {
+			//$('#results').html(data);
+			alert('Данные отправлены.');
+		},
+		error:  function(xhr, str){
+			var err = ('Ошибка '+ xhr.status +': ');
+			if (xhr.status == 404) {
+				//alert(xhr.responseCode + ' ' + str);
+				alert(err + 'Сервер не отвечает');
+			} else if (xhr.status == 400) {
+				alert(err + 'Ошибка в запросе');
+			} else {
+				alert(err + 'Неизвестная ошибка');
+			}			
+		}
+	});
+}
+	
+	
